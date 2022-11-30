0ae9e36b63cbf14d5421604f62076b40607c6326
Diego da Silva Souza <linediego123@gmail.com>
2022-11-30 14:39:30 -0300

    sucess final done..

--- gitcard.js
+++ gitcard.js
@@ -1,28 +1,34 @@
-let user = document.querySelector("usuario-github");
-let botaoBuscar = document.querySelector("buscar-github");
-const link = document.querySelector("a");
+let usr = document.querySelector("#usuario-github");
+let btn = document.querySelector("#buscar-github");
+let link = document.querySelector("a");
 
 let avatar_link = document.querySelector(".avatar");
 let avatar_img = avatar_link.querySelector("img");
-let content = document.querySelector(".content b1");
-let total_repo = document.querySelector(".status li a strong")[0];
-let total_glist = document.querySelector(".status li a strong")[1];
-let total_seg = document.querySelector(".status li a strong")[2];
+let nome = document.querySelector(".content h1");
+let total_repo = document.querySelectorAll(".status li a strong")[0];
+let total_gist = document.querySelectorAll(".status li a strong")[1];
+let total_seg = document.querySelectorAll(".status li a strong")[2];
+
 
 const getGitHubInfo = function (username) {
-    var url = 'https://api.github.com/users/' + username;
+
+    var url = `https://api.github.com/users/${username}`;
+
     var ajax = new XMLHttpRequest();
+    
     ajax.onreadystatechange = function () {
+
         if(this.readyState == 4 && this.status == 200){
             let ajax = JSON.parse(this.responseText);
-            content.innerText = ajax.name;
-            total_seg.innerText=ajax.followers
-            total_repo.innerText=ajax.public_repos
-            total_glist.innerText=ajax.public_gists
-            avatar_img.src=ajax.avatar_url;
-            link_href = ajax.html_url;
-            console.log(ajax)
+            nome.innerText = ajax.name;
+            total_seg.innerText = ajax.followers;
+            total_repo.innerText = ajax.public_repos;
+            total_gist.innerText = ajax.public_gists;
+            avatar_img.src = ajax.avatar_url;
+            link.href = ajax.html_url;
+            console.log(ajax);
         }
+        
     };
 
     ajax.open('GET', url, true);
@@ -30,7 +36,7 @@ const getGitHubInfo = function (username) {
 
 };
 
-botaoBuscar.addEventListener("click" , e =>{
+btn.addEventListener("click", e => {
     e.preventDefault();
-    getGitHubInfo(user, value);
+    getGitHubInfo(usr.value);
 })
